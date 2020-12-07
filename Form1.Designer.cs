c741507f3afb93f424df1b1314c683b2a88470b4
kelbv <kelvin.tooth@gmail.com>
2020-12-07 18:35:48 +0000

    added icon

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -72,6 +73,7 @@
             this.ClientSize = new System.Drawing.Size(733, 571);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Sine Cosine and Tan";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
